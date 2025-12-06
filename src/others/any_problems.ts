99c459ed6eed25ec8398d90067af8a58ca198b3a
nao-United92 <naoyauver@icloud.com>
2025-12-06 12:07:19 +0900

    delete unnecessary comments

--- src/others/any_problems.ts
+++ src/others/any_problems.ts
@@ -7,12 +7,10 @@ import { UserResponse } from './app-types'
 // 2. `processData` 関数の引数と戻り値の型を、`any` ではなく適切な型に修正しましょう。
 // 3. `data` の中の `tags` が文字列の配列であることを型で表現してみましょう。
 
-// レスポンスデータがany型で定義されている
 export function processData(data: UserResponse): { userId: number; tags: string[] } {
   console.log(`Processing data for user: ${data.user.name}`)
 
   // data.details.tags の中身を大文字に変換する
-  // しかし、これでは tags が本当に存在するか、配列であるかが保証されない
   const processedTags = data.details.tags.map((tag: string) => tag.toUpperCase())
 
   return {
@@ -36,6 +34,4 @@ const responseData: UserResponse = {
 const result = processData(responseData)
 console.log(result)
 
-// 以下のコードは、data.user.name が string 型だと推論できないため、エディタ上でエラーとなるはずです。
-// anyをなくすことで、このような問題を解決できます。
 console.log(result.userId.toFixed(2))
